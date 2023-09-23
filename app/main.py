7719843e113e8ade1cc432506379f9108af173a2
Endy B <endyb.dev@gmail.com>
2023-09-23 15:23:57 +1000

    App context

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 import os
 
 from fastapi import FastAPI
+from fastapi import Request
 
-app = FastAPI()
+import asyncio
+
+# LLM
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Text generation
+from langchain.llms import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
+
+import copy
+
+
+app = FastAPI(
+    title='API LLM LaMini-Flan-T5-77M K8s',
+    description= "API chatbot that allow you to use MBZUAI/LaMini-Flan-T5-77M locally/on-premise or in a secure environment"
+    version='v0.0.1'
+)
 
 @app.get("/")
 def hello():
